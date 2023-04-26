1e9e1bd5bca91ff78fe224d8b5083ae22b9bdf5b
LeshuySMB <75433783+LeshuySMB@users.noreply.github.com>
2023-04-26 23:37:28 +0300

    bot can send a group of pics, like instrucrion

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import telebot
+import os
 
 
-with open('token.txt', 'r', encoding='utf-8') as f:
+with open(f'{os.path.dirname(os.path.abspath(__file__))}\\token.txt', 'r', encoding='utf-8') as f:
     for line in f:
         token = line
 
@@ -11,13 +12,23 @@ bot = telebot.TeleBot(token)
 def pr(chat_id):
     bot.send_message(chat_id, "Kon'")
 
+def parse_imgs():
+    imgs = []
+    path = os.path.dirname(os.path.abspath(__file__))+'\\pics'
+    for img in os.listdir(path):
+        imgs.append(telebot.types.InputMediaPhoto(open(path+f'\\{img}', 'rb')))
+    return(imgs)
+
+
+
 #Хендлер для начала работы с ботом    
 @bot.message_handler(commands='start')
 def start_mess(message):
     bot.send_message(message.chat.id, "Привет! Это проект ASMT.")
     mess = "Я бот-анализатор, и для моей работы нужен json-файл\n На скриншотах представлена инструкция как это сделать."
     bot.send_message(message.chat.id, mess)
-    bot.send_photo(message.chat.id, photo=open('photo/pic_1.png', 'rb'))
+    imgs = parse_imgs()
+    bot.send_media_group(message.chat.id, media=imgs)
 
 #Хендлер для вывода расписания
 
